30b96c1fe7679fed202badf8dcdd6f8e18465f91
Fabricio Juncal <juncalfabri@gmail.com>
2017-08-03 01:01:13 -0300

    Add files via upload

---
+++ Actividades python/Registro de Usuarios con BD MySql/Create Table.sql
@@ -0,0 +1,11 @@
+USE prueba;
+
+CREATE TABLE Usuarios (
+
+id_usuarios INT (10) NOT NULL AUTO_INCREMENT,
+Nombres VARCHAR(100) NOT NULL,
+Apellidos VARCHAR(100) NOT NULL,
+Edad INT(3) NOT NULL
+PRIMARY KEY (id_usuario
+
+);
