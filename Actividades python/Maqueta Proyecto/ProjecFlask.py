25975b4c56be36e6eda80c4a5071b3b163433b53
Fabricio Juncal <juncalfabri@gmail.com>
2017-10-22 14:17:28 -0300

    Update ProjecFlask.py

--- Actividades python/Maqueta Proyecto/ProjecFlask.py
+++ Actividades python/Maqueta Proyecto/ProjecFlask.py
@@ -1,4 +1,5 @@
 from Luz import *
+from Servo import *
 import virtualenv
 from flask import Flask
 
@@ -7,9 +8,14 @@ app = Flask(__name__)
 
 Luces = {} # Crear un diccionario para almacenar y hacer globales las instancias.
 
+servos={}# se crea dicionarios de los servos.
+
+
+#    Control de Luz
+
 @app.route('/')
 def ControlLuces():
-		return 'Controlando Luces'
+		return 'Controlando Luces y Servos'
 
 @app.route('/NuevaLuz/<int:Nid>')
 def NuevaLuz(Nid):
@@ -18,7 +24,7 @@ def NuevaLuz(Nid):
 	print Luces[Nid]    # Muestra en la consola la instancia 
 	return ("Luz  L%s Creada!!" % Nid)
 
-@app.route('/Prender/<int:Nid>')
+@app.route('/Luz/Prender/<int:Nid>')
 def PrenderLuz(Nid):
 	# Prende la luz seleccionada mediante el "id" (Nid)
 	try:
@@ -33,7 +39,7 @@ def PrenderLuz(Nid):
 		return("Ah ocurrido un error inesperado, intente otra vez")
 	
 	
-@app.route('/Apagar/<int:Nid>')
+@app.route('/Luz/Apagar/<int:Nid>')
 def ApagarLuz(Nid):
 	# Apaga la luz seleccionada mediante el "id" (Nid)
 	try:
@@ -46,6 +52,50 @@ def ApagarLuz(Nid):
 	
 	except:
 		return("Ah ocurrido un error inesperado, intente otra vez")
+#----------------------------------------------------------------------------------------------------------------------------------------------------------	
+
+#     Control de Servo
+
+@app.route('/NuevoServo/<int:Nid>') #se crea la instancia de servo con el id
+def NuevoServo(Nid):
+    servos[Nid]=Servo(Nid)
+    return ('Servo ID %s  creado' %Nid)
+
+@app.route('/Servo/<int:Nid>/Mover/<int:angulo>')  # se mueve el servo numero id en un angulo especifico.
+def mover(Nid,angulo):
+	try:
+			mensaje=servos[Nid].mover(angulo)
+			print mensaje
+			return ('Servo ID %s se movio %s grados' %(Nid, angulo))
+	except KeyError:
+			return ("Ha ocurrido un error, no exite servo con el ID: %s" %Nid)
+	except:
+			return ("Ha ocurrido un error inesperado, intente otra vez")	
+
+			
+@app.route('/Servo/Abrir/<int:Nid>')  # se abre el servo y quedaria en el angulo 90			
+def abrir(Nid):
+	try:
+			mensaje=servos[Nid].abrir()
+			print mensaje
+			return ('Servo ID %s  se abrio' %Nid)
+	except KeyError:
+			return ("Ha ocurrido un error, no exite servo con el ID: %s" %Nid)
+	except:
+			return ("Ha ocurrido un error inesperado, intente otra vez")
+
 	
+@app.route('/Servo/Cerrar/<int:Nid>')	 # se cierra el servo y quedaria en el angulo 0		
+def cerrar(Nid):
+	try:
+			mensaje=servos[Nid].cerrar()
+			print mensaje
+			return ('Servo ID %s se cerro' %Nid)
+	except KeyError:
+			return ("Ha ocurrido un error, no exite servo con el ID: %s" %Nid)
+	except:
+			return ("Ha ocurrido un error inesperado, intente otra vez")	
+			
+			
 if __name__ == '__main__':
-	app.run( host="0.0.0.0",port=int("5000"))
+	app.run( host="0.0.0.0",port=int("3000"))
